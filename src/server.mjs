39fedb0a0e449d471ab016cb029ae8f5b6ee72fa
Aiden Bai <mail.contraband@gmail.com>
2021-04-08 21:51:23 -0700

    Remove unused dependencies

--- src/server.mjs
+++ src/server.mjs
@@ -2,12 +2,10 @@ import dotenv from 'dotenv';
 import express from 'express';
 import boom from 'express-boom';
 import cors from 'cors';
-import session from 'express-session';
 import compression from 'compression';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import dirname from 'es-dirname';
-import { Database } from 'sqlite3';
 import { join } from 'path';
 
 import baseRoutes from './routes/base.mjs';
@@ -18,18 +16,13 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', join(dirname(), './../views'));
 
-
-
 app.use(boom());
 app.use(cors());
 app.use(compression());
 app.use(helmet());
-app.use(session({ secret: process.env.SECRET }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
-
-passport.use(strategy);
 
 app.use('/', baseRoutes);
 
